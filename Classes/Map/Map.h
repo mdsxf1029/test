3ac006ae199bffa6a82367f016eb7b08abe45732
Yingzhengxinjianchong <2219704394@qq.com>
2024-11-27 07:30:58 +0800

    更新地图类设计

---
+++ Classes/Map/Map.h
@@ -0,0 +1,29 @@
+#pragma once
+class Map
+{
+public:
+	// 请补充大小地图之间切换的函数
+
+	/*函数名：SwitchBetweenSmallMaps
+	  函数功能：小地图之间锚点的切换
+	  参数列表：
+		  role_coord_x，role_coord_y：人物当前所在坐标，通过交互系统获取，从域外地图跳回时需要用到。
+		  object_map：需要跳转的对象，通过交互系统获取，决定跳转到哪个地图去。
+	  返回值：是否成功跳转，成功则为true
+	*/
+	bool SwitchBetweenSmallMaps(const int& role_coord_x, const int& role_coord_y, const int& object_map);
+
+	/*函数名：PanM
+	  函数功能：地图展示时的平移
+	  参数列表：
+		  role_coord_x，role_coord_y：人物当前所在坐标，通过交互系统获取。
+	  返回值：无
+	*/
+	void Pan(const int& role_coord_x, const int& role_coord_y);
+
+	// 请补充地图缩放函数
+
+private:
+	vector<int> before_jump_point; // 保存跳转之前的人物坐标
+	bool fog; // 是否存在迷雾，有迷雾为true
+	vector<> special_points; // 结构体数组，存储特殊点坐标，类型：出生点0，怪兽1，迷宫2，钓鱼3，采药4，小动物5
