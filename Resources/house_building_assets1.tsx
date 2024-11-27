0a4eff2d444408beae1c4f4332e075c3eafdfd27
mdsxf1029 <145674548+mdsxf1029@users.noreply.github.com>
2024-11-27 12:03:36 +0800

    Delete Resources/house_building_assets1.tsx

--- Resources/house_building_assets1.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.0" name="house_building_assets" tilewidth="16" tileheight="16" tilecount="2432" columns="38">
- <image source="C:/Users/LENOVO/Projects/Bugs-Bugger-Off/MyGame/Resources/TiledMap/SmallBurg_village_pack_v3.18_free/assets/housing/old version/house_building_assets.png" width="608" height="1024"/>
-</tileset>
