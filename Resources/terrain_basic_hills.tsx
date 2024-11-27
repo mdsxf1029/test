d369b60f45b460ee215f421bf2c7ab1c44db0e83
mdsxf1029 <145674548+mdsxf1029@users.noreply.github.com>
2024-11-27 12:05:34 +0800

    Delete Resources/terrain_basic_hills.tsx

--- Resources/terrain_basic_hills.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.0" name="terrain_basic_hills" tilewidth="16" tileheight="16" tilecount="351" columns="27">
- <image source="C:/Users/LENOVO/Projects/Bugs-Bugger-Off/MyGame/Resources/TiledMap/SmallBurg_village_pack_v3.18_free/assets/tileset_v3_wip/terrain_basic_hills.png" width="432" height="208"/>
-</tileset>
