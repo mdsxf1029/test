3768134b26dea4341e6c2260fef71c5156837e06
mdsxf1029 <145674548+mdsxf1029@users.noreply.github.com>
2024-11-27 12:04:38 +0800

    Delete Resources/house_interiors_assets-export.tsx

--- Resources/house_interiors_assets-export.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.0" name="house_interiors_assets-export" tilewidth="16" tileheight="16" tilecount="2048" columns="64">
- <image source="C:/Users/LENOVO/Projects/Bugs-Bugger-Off/MyGame/Resources/TiledMap/SmallBurg_village_pack_v3.18_free/assets/housing/house_interiors_assets-export.png" width="1024" height="512"/>
-</tileset>
