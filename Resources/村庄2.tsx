e14e825391f1cafab9156bdbfbac04215f4b19ee
mdsxf1029 <145674548+mdsxf1029@users.noreply.github.com>
2024-11-27 12:06:12 +0800

    Delete Resources/村庄2.tsx

--- Resources/村庄2.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.0" name="村庄2" tilewidth="16" tileheight="16" tilecount="270" columns="27">
- <image source="C:/Users/LENOVO/Projects/Bugs-Bugger-Off/MyGame/Resources/TiledMap/SmallBurg_village_pack_v3.18_free/assets/tileset_v2/outside_assets_stairs.png" width="432" height="160"/>
-</tileset>
