66f13b2ca30bfec03e7006ca85e1c12d190278b6
mdsxf1029 <145674548+mdsxf1029@users.noreply.github.com>
2024-11-27 12:04:20 +0800

    Delete Resources/house_doors.tsx

--- Resources/house_doors.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.0" name="house_doors" tilewidth="16" tileheight="16" tilecount="1504" columns="47">
- <image source="C:/Users/LENOVO/Projects/Bugs-Bugger-Off/MyGame/Resources/TiledMap/SmallBurg_village_pack_v3.18_free/assets/housing/house_doors.png" width="752" height="512"/>
-</tileset>
