e738b5dc4457abf9af049d93a9f881e5e7295a43
mdsxf1029 <145674548+mdsxf1029@users.noreply.github.com>
2024-11-27 12:04:29 +0800

    Delete Resources/house_building_walls_shadows.tsx

--- Resources/house_building_walls_shadows.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.0" name="house_building_walls_shadows" tilewidth="16" tileheight="16" tilecount="4005" columns="89">
- <image source="C:/Users/LENOVO/Projects/Bugs-Bugger-Off/MyGame/Resources/TiledMap/SmallBurg_village_pack_v3.18_free/assets/housing/old version/house_building_walls_shadows.png" width="1424" height="721"/>
-</tileset>
