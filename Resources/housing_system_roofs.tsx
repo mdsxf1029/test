98e6b0a4290ab35d12d6e8d541e65da405da5ee3
mdsxf1029 <145674548+mdsxf1029@users.noreply.github.com>
2024-11-27 12:05:16 +0800

    Delete Resources/housing_system_roofs.tsx

--- Resources/housing_system_roofs.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.0" name="housing_system_roofs" tilewidth="16" tileheight="16" tilecount="3300" columns="75">
- <image source="C:/Users/LENOVO/Projects/Bugs-Bugger-Off/MyGame/Resources/TiledMap/SmallBurg_village_pack_v3.18_free/assets/housing/roofs/housing_system_roofs.png" width="1200" height="704"/>
-</tileset>
