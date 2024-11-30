f296fd2dc6028ee0f29f048946b753c54bcb06bf
ln Q <152588716+qianlinnian@users.noreply.github.com>
2024-12-01 00:47:10 +0800

    Add files via upload
    set_variables

---
+++ Classes/sets_variables.h
@@ -0,0 +1,102 @@
+#pragma once
+#ifndef SET_VARIABLES_H
+#define SET_VARIABLES_H
+
+#include<string>
+#include<string.h>
+
+#include"items.h"
+#include"player.h"
+#include"tasks.h"
+#include"skills.h"
+#include"npc.h"
+#include"elements.h"
+#include"inventory.h"
+
+
+//玩家类
+
+extern Player hero;
+
+//任务
+
+extern Task mainTask;//主线任务
+extern Task sideTaskOne;//副任务1
+extern Task sideTaskTwo;//副任务2	
+extern Task sideTaskThree;//副任务3
+extern Task sideTaskFour;//副任务4
+extern Task sideTaskFive;//副任务5
+extern Task sideTaskSix;//副任务6
+extern Task sideTaskSeven;//副任务7
+extern Task nonTask;//无任务
+
+//技能
+
+extern LowLevelSkill elementSurge;//元素冲击
+extern MidLevelSkill elementalTorrent;//元素洪流
+extern HighLevelSkill energyVortex;//能量漩涡
+//组合技能
+extern HighLevelSkill arcaneBlast;//奥术爆发
+
+
+//NPC
+
+//友方NPC
+extern FriendNpc princess;//公主
+extern FriendNpc king;//国王
+extern FriendNpc minister;//大臣
+
+extern FriendNpc clothesProvider;//服装商
+extern FriendNpc weaponProvider;//武器商
+extern FriendNpc foodProvider;//食品商
+extern FriendNpc skillProvider;//技能商
+
+
+//怪物数量后期看看怎么处理
+extern EnemyNpc highLevelFireMonster;//高级火怪物
+extern EnemyNpc lowLevelFireMonster;//低级火怪物
+
+extern EnemyNpc highLevelWaterMonster;//高级水怪物
+extern EnemyNpc lowLevelWaterMonster;//低级水怪物
+
+extern EnemyNpc highLevelWoodMonster;//高级木怪物
+extern EnemyNpc lowLevelWoodMonster;//低级木怪物
+
+extern EnemyNpc highLevelGoldMonster;//高级金怪物
+extern EnemyNpc lowLevelGoldMonster;//低级金怪物
+
+extern EnemyNpc highLevelEarthMonster;//高级土怪物
+extern EnemyNpc lowLevelEarthMonster;//低级土怪物
+
+//物品
+
+//武器 变量
+extern Weapon  magicRing;
+extern Weapon  magicCrystal;
+extern Weapon  magicScroll;
+
+//护具 变量
+extern Armor helmet;
+extern Armor armor;
+extern Armor shoes;
+
+//食品 变量
+extern Food fish;
+extern Food fruit;
+extern Food vegetable;
+extern Food herb;
+extern Food meat;
+
+//材料 变量
+extern Material tree;
+extern Material stone;
+
+//特殊物品 变量
+extern TaskItem key;
+extern TaskItem letter;
+ 
+
+//背包
+extern Inventory bag;//背包
+
+#endif // !SET_VARIABLES_H
---
+++ Classes/skills.cpp
@@ -0,0 +1,26 @@
+#include"skills.h" 
+#include"sets_variables.h"
+//技能类
+Skill::Skill(const std::string name) :name(name) {};
+Skill::~Skill() {};
+
+//低级技能
+LowLevelSkill::LowLevelSkill(std::string name) :Skill(name)
+{
+	attack = 10;
+};
+LowLevelSkill::~LowLevelSkill() {};
+
+//中级技能
+MidLevelSkill::MidLevelSkill(std::string name) :Skill(name)
+{
+	attack = 20;
+};
+MidLevelSkill::~MidLevelSkill() {};
+
+//高级技能
+HighLevelSkill::HighLevelSkill(std::string name) :Skill(name)
+{
+	attack = 30;
+};
+HighLevelSkill::~HighLevelSkill() {};
