d6bf38356786961422714b706ebcd1cd5dbca193
ln Q <152588716+qianlinnian@users.noreply.github.com>
2024-12-23 02:00:57 +0800

    Delete gold1.tsx错误位置

--- gold1.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.0" name="gold1" tilewidth="16" tileheight="16" tilecount="20160" columns="180">
- <image source="gold1.png" width="2880" height="1800"/>
-</tileset>
